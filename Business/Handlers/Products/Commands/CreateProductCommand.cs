411d54b76223cb3e16c94ee71012583a2903c43d
uzeyirbulentsahin <uzeyir.bulent.sahin@gmail.com>
2021-03-11 13:58:56 +0300

    Add project files.

---
+++ Business/Handlers/Products/Commands/CreateProductCommand.cs
@@ -0,0 +1,61 @@
+﻿
+using Business.BusinessAspects;
+using Business.Constants;
+using Core.Aspects.Autofac.Caching;
+using Core.Aspects.Autofac.Logging;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Logging.Serilog.Loggers;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
+using Business.Handlers.Products.ValidationRules;
+
+namespace Business.Handlers.Products.Commands
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class CreateProductCommand : IRequest<IResult>
+    {
+
+        public string Name { get; set; }
+
+
+        public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, IResult>
+        {
+            private readonly IProductRepository _productRepository;
+            private readonly IMediator _mediator;
+            public CreateProductCommandHandler(IProductRepository productRepository, IMediator mediator)
+            {
+                _productRepository = productRepository;
+                _mediator = mediator;
+            }
+
+            [ValidationAspect(typeof(CreateProductValidator), Priority = 1)]
+            [CacheRemoveAspect("Get")]
+            [LogAspect(typeof(FileLogger))]
+            [SecuredOperation(Priority = 1)]
+            public async Task<IResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+            {
+                var isThereProductRecord = _productRepository.Query().Any(u => u.Name == request.Name);
+
+                if (isThereProductRecord == true)
+                    return new ErrorResult(Messages.NameAlreadyExist);
+
+                var addedProduct = new Product
+                {
+                    Name = request.Name,
+
+                };
+
+                _productRepository.Add(addedProduct);
+                await _productRepository.SaveChangesAsync();
+                return new SuccessResult(Messages.Added);
+            }
+        }
+    }
+}
---
+++ Business/Handlers/Products/Commands/UpdateProductCommand.cs
@@ -0,0 +1,57 @@
+﻿
+using Business.Constants;
+using Business.BusinessAspects;
+using Core.Aspects.Autofac.Caching;
+using Core.Aspects.Autofac.Logging;
+using Core.CrossCuttingConcerns.Logging.Serilog.Loggers;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
+using Core.Aspects.Autofac.Validation;
+using Business.Handlers.Products.ValidationRules;
+
+
+namespace Business.Handlers.Products.Commands
+{
+
+
+    public class UpdateProductCommand : IRequest<IResult>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public class UpdateProductCommandHandler : IRequestHandler<UpdateProductCommand, IResult>
+        {
+            private readonly IProductRepository _productRepository;
+            private readonly IMediator _mediator;
+
+            public UpdateProductCommandHandler(IProductRepository productRepository, IMediator mediator)
+            {
+                _productRepository = productRepository;
+                _mediator = mediator;
+            }
+
+            [ValidationAspect(typeof(UpdateProductValidator), Priority = 1)]
+            [CacheRemoveAspect("Get")]
+            [LogAspect(typeof(FileLogger))]
+            [SecuredOperation(Priority = 1)]
+            public async Task<IResult> Handle(UpdateProductCommand request, CancellationToken cancellationToken)
+            {
+                var isThereProductRecord = await _productRepository.GetAsync(u => u.Id == request.Id);
+
+
+                isThereProductRecord.Name = request.Name;
+
+
+                _productRepository.Update(isThereProductRecord);
+                await _productRepository.SaveChangesAsync();
+                return new SuccessResult(Messages.Updated);
+            }
+        }
+    }
+}
+
